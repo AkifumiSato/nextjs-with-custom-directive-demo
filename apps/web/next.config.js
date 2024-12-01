0663a9a22c128b13fa5910e788c2bc96b5634257
佐藤昭文 <akfm.sato@gmail.com>
2024-12-01 22:32:31 +0900

    swc pluginの追加

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    swcPlugins: [["../../packages/swc-plugin-use-debug", {}]],
+  },
+};
 
 export default nextConfig;

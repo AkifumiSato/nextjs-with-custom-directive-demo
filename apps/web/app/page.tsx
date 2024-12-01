e7a98a563d4596a3b80418fa86c645838e1041e9
佐藤昭文 <akfm.sato@gmail.com>
2024-12-01 23:09:09 +0900

    `"use debug"`ディレクティブを判別してconosoleを出力

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
+  logging("test", 999);
+
   return (
     <div>
       <h1>Next.js with custom directive DEMO</h1>
     </div>
   );
+}
+
+function logging(..._arg: unknown[]) {
+  "use debug";
 }
--- packages/swc-plugin-use-debug/src/lib.rs
+++ packages/swc-plugin-use-debug/src/lib.rs
@@ -1,39 +1,84 @@
+use swc_core::common::{SyntaxContext, DUMMY_SP};
+use swc_core::ecma::visit::VisitMutWith;
 use swc_core::ecma::{
-    ast::Program,
+    ast::*,
     transforms::testing::test_inline,
     visit::{visit_mut_pass, VisitMut},
 };
-use swc_core::ecma::visit::VisitMutWith;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 
 pub struct TransformVisitor;
 
+// https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
 impl VisitMut for TransformVisitor {
-    // Implement necessary visit_mut_* methods for actual custom transform.
-    // A comprehensive list of possible visitor methods can be found here:
-    // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
-    // fn visit_mut_function(&mut self, f: &mut Function) {
-    //
-    // }
+    // todo: arrow function
+    fn visit_mut_function(&mut self, f: &mut Function) {
+        let Some(body) = &mut f.body else {
+            // 呼び出し元の関数bodyがない場合は終了
+            return;
+        };
+
+        // 関数ボディの先頭で "use debug"; を探す
+        if let Some(Stmt::Expr(ExprStmt { expr, .. })) = body.stmts.get(0) {
+            if let Expr::Lit(Lit::Str(Str { value, .. })) = &**expr {
+                // todo: 以下を参考にDirectiveを取得するようリファクタ
+                // https://github.com/vercel/next.js/blob/564794df56e421d6d4c2575b466a8be3a96dd39a/crates/next-custom-transforms/src/transforms/server_actions.rs#L339
+                if value == "use debug" {
+                    // console.log を挿入
+                    // `process.env.NODE_ENV`などで分岐したいが、サンプルなので省略
+                    let log_stmt = Stmt::Expr(ExprStmt {
+                        span: DUMMY_SP,
+                        expr: Box::new(Expr::Call(CallExpr {
+                            span: DUMMY_SP,
+                            callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
+                                span: DUMMY_SP,
+                                obj: Box::new(Expr::Ident(Ident::new(
+                                    "console".into(),
+                                    DUMMY_SP,
+                                    SyntaxContext::empty(),
+                                ))),
+                                prop: MemberProp::Ident(IdentName::new("log".into(), DUMMY_SP)),
+                            }))),
+                            args: vec![
+                                ExprOrSpread {
+                                    spread: None,
+                                    expr: Box::new(Expr::Lit(Lit::Str(Str {
+                                        span: DUMMY_SP,
+                                        value: "use debug".into(),
+                                        raw: None,
+                                    }))),
+                                },
+                                ExprOrSpread {
+                                    spread: None,
+                                    expr: Box::new(Expr::Ident(Ident::new(
+                                        "arguments".into(),
+                                        DUMMY_SP,
+                                        SyntaxContext::empty(),
+                                    ))),
+                                },
+                            ],
+                            type_args: None,
+                            ..Default::default()
+                        })),
+                    });
+
+                    // "use debug"; を削除して console.log を挿入
+                    body.stmts.remove(0);
+                    body.stmts.insert(0, log_stmt);
+                }
+            }
+        }
+
+        // 子ノードの変換を続行
+        body.visit_mut_with(self);
+    }
 }
 
-/// An example plugin function with macro support.
-/// `plugin_transform` macro interop pointers into deserialized structs, as well
-/// as returning ptr back to host.
-///
-/// It is possible to opt out from macro by writing transform fn manually
-/// if plugin need to handle low-level ptr directly via
-/// `__transform_plugin_process_impl(
-///     ast_ptr: *const u8, ast_ptr_len: i32,
-///     unresolved_mark: u32, should_enable_comments_proxy: i32) ->
-///     i32 /*  0 for success, fail otherwise.
-///             Note this is only for internal pointer interop result,
-///             not actual transform result */`
-///
-/// This requires manual handling of serialization / deserialization from ptrs.
-/// Refer swc_plugin_macro to see how does it work internally.
 #[plugin_transform]
-pub fn process_transform(mut program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+pub fn process_transform(
+    mut program: Program,
+    _metadata: TransformPluginProgramMetadata,
+) -> Program {
     program.visit_mut_with(&mut visit_mut_pass(TransformVisitor));
     program
 }
@@ -47,7 +92,13 @@ test_inline!(
     |_| visit_mut_pass(TransformVisitor),
     boo,
     // Input codes
-    r#"console.log("transform");"#,
+    r#"
+function boo() {
+  "use debug";
+}"#,
     // Output codes after transformed with plugin
-    r#"console.log("transform");"#
+    r#"
+function boo() {
+  console.log("use debug", arguments);
+}"#
 );
